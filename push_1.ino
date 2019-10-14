1c4ad44c1f962a6b892b3f8b60fa863296996a76
Anmol0212 <56043213+Anmol0212@users.noreply.github.com>
2019-10-15 00:17:17 +0530

    Update push_1.ino

--- push_1.ino
+++ push_1.ino
@@ -1,7 +1,19 @@
 
 int VAL=0;
-int p1=5;
+int p1=3;
 int p2=7;
+const int m1  = 5;    
+const int m2  = 6;    
+//Motor B
+const int m3  = 9;  
+const int m4  = 8;
+
+
+int r = 2;
+int RainVal;
+int IsRaining;
+String strRaining;
+ 
 
 void setup(){
   
@@ -11,6 +23,13 @@ void setup(){
   
   pinMode(p1,OUTPUT);
   pinMode(p2,OUTPUT);
+  Serial.begin(9600);
+  pinMode(2,INPUT);
+
+     pinMode(m1, OUTPUT);
+    pinMode(m2, OUTPUT);
+    pinMode(m3, OUTPUT);
+    pinMode(m4, OUTPUT);
 }
      void ledstate(int pin, int state) 
     {
